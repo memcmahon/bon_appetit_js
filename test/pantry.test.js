dc0286961dd9ab74e7049b4bef12966caa59a269
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-04 10:25:37 -0600

    Adds test for checkStock

--- test/pantry.test.js
+++ test/pantry.test.js
@@ -3,10 +3,24 @@ import Pantry from "../lib/pantry"
 
 describe("Pantry", () => {
   describe("attributes", () => {
-    const pantry = new Pantry()
+    const pantry = new Pantry();
 
     it("exists", () => {
       expect(pantry).instanceOf(Pantry);
+    });
+
+    it("has stock", () => {
+      expect(pantry.stock).to.deep.equal({});
+    })
+  });
+
+  describe("functions", () => {
+    describe("checkStock", () => {
+      const pantry = new Pantry();
+
+      it("can check if something is in the pantry", () => {
+        expect(pantry.checkStock("Cheese")).to.equal(0);
+      });
     });
   });
 });
